036412b4b10e0bdc39b0ea73a8285caa44e1436e
Jonathan North Washington <jonathan.n.washington@gmail.com>
2014-06-18 15:16:02 +0000

    % fix

--- dev/kaa.Cyrl-Latn_1991.sh
+++ dev/kaa.Cyrl-Latn_1991.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hfst-strings2fst  | hfst-compose-intersect -2 kaa.Cyrl-Latn_1991.hfst  | hfst-fst2strings
+sed 's/:/\\:/g' | hfst-strings2fst  | hfst-compose-intersect -2 kaa.Cyrl-Latn_1991.hfst  | hfst-fst2strings
